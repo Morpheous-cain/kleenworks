34fd76d4cc31cdd382bbcd32fdd9b11ee35656ae
immersitech-consultancy <immersitec@gmail.com>
2026-07-08 01:06:27 +0300

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
           } catch(e) {}
         `}} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <meta name="google-site-verification" content="suvx2vxyLhZLSugMutRGHvhuFdGsJnAZkOYGltmGWRk" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
